ddeb23759e8fe86109dd429f1dc9659c7ef5525e
Fiifi Ofori <fiifi56@gmail.com>
2022-02-03 18:44:25 +0000

    changing screen font family

--- lib/widgets/footer.dart
+++ lib/widgets/footer.dart
@@ -20,6 +20,7 @@ class Footer extends StatelessWidget {
           style: TextStyle(
             color: Color(0xff5687eb),
             fontSize: 13.0,
+            fontFamily: 'Nunito'
           ),
         ),
         SizedBox(width: 2.0),
@@ -29,6 +30,7 @@ class Footer extends StatelessWidget {
             color: Colors.white,
             fontSize: 13.0,
             fontWeight: FontWeight.bold,
+              fontFamily: 'Nunito'
           ),
         ),
       ],
