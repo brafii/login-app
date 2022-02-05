0d67b76aa5dd190fe150d7d357c87820422ffdb4
Fiifi Ofori <fiifi56@gmail.com>
2022-02-05 21:57:01 +0000

    completed parcel history page

--- lib/widgets/sign_in.dart
+++ lib/widgets/sign_in.dart
@@ -16,6 +16,52 @@ class SignIn extends StatelessWidget {
           decoration: BoxDecoration(
             color: defaultColors,
           ),
+          child: Column(
+            children: [
+              Expanded(
+                flex: 3,
+                child: Column(
+                  children: [
+                    Text(
+                      'Sign in',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 5,
+                child: Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20.0),
+                      topRight: Radius.circular(20.0),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(25.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Welcome Back',
+                          style: TextStyle(
+                            fontFamily: 'Nunito',
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20.0,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
